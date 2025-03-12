36f7762758c139140cb92e9abeb46b5ce95a9613
Jaireua <jairenriquez30@gmail.com>
2025-03-12 16:10:49 -0500

    creating urls tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import TaskSerializer
+from .models import Task
 
 # Create your views here.
+def task_view(request):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
