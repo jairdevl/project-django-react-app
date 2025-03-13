b41365d049db6bc5826630b59aea4d7689c1168a
Jaireua <jairenriquez30@gmail.com>
2025-03-13 09:17:42 -0500

    modified urls

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,6 @@ from .serializer import TaskSerializer
 from .models import Task
 
 # Create your views here.
-def task_view(request):
+class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
