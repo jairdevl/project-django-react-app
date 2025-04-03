b883767b0e6e3cd36672190394ceeb90c12faa90
Jaireua <jairenriquez30@gmail.com>
2025-04-03 15:06:12 -0500

    create tasks

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
-export const getAllTasks = () => {
-    return axios.get("http://localhost:8000/tasks/api/v1/tasks/")
-}
+const tasksapi = axios.create({
+    baseURL: 'http://localhost:8000/tasks/api/v1/tasks/'
+})
+
+export const getAllTasks = () => tasksapi.get();
+
+export const createTask = (task) => tasksapi.post("", task);
+
--- client/src/pages/TasksFormPage.jsx
+++ client/src/pages/TasksFormPage.jsx
@@ -1,7 +1,27 @@
+import { useForm } from 'react-hook-form'
+import { createTask } from '../api/tasks.api'
+import {useNavigate } from 'react-router-dom'
+
 export function TasksFormPage() {
-    return (
-      <div>
-        Tasks Form Page
-      </div>
-    )
-  }
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const navigate = useNavigate()
+
+  const onSubmit = handleSubmit(async data => {
+    await createTask(data);
+    navigate("/tasks");
+  })
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="title"
+          {...register("title", { required: true })} />
+          {errors.title && <span>Title is required</span>}
+        <textarea rows="3" placeholder="description"
+          {...register("description", { required: true })}></textarea>
+          {errors.description && <span>Description is required</span>}
+        <button>Save</button>
+      </form>
+    </div>
+  )
+}
